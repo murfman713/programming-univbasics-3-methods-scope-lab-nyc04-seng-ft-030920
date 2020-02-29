6c6b54d2f7b39b282e1ffa07a3c5bed6ea7da3ae
evan kruger <ekruger713@gmail.com>
2020-02-29 20:12:31 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,8 +13,14 @@ def link
   puts phrase
 end
 
+<<<<<<< HEAD
 def any_phrase(phrase)
     puts "#{phrase}"
 end
 
 any_phrase("Do A Barrel Roll!\n")
+=======
+def any_phrase(phrase = "Do A Barrel Roll!")
+    puts #{phrase}
+end
+>>>>>>> 4c7f9f36044843902392e81acd1d0708449d0c83
